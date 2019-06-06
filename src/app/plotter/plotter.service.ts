3b2b34fda6d7f51fd0cc0cdc66d666ba54828ff4
Brent Miller <brentwmiller93@gmail.com>
2019-06-05 23:36:48 -0400

    Adds create plot; updates how grid is saved for now. Will be attached to a plot

--- src/app/plotter/plotter.service.ts
+++ src/app/plotter/plotter.service.ts
@@ -9,25 +9,46 @@ import { AuthService } from '../shared/services/auth.service';
 // interfaces
 import { Box } from './interfaces/box.interface';
 import { User } from '../shared/interfaces/user.interface';
+import { Plot } from './interfaces/plot.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlotterService {
-  constructor(private authService: AuthService, private db: AngularFirestore) {}
+  uid: string;
+
+  constructor(private authService: AuthService, private db: AngularFirestore) {
+    this.uid = authService.user.uid;
+  }
+
+  createPlot(plot: Plot) {
+    this.db
+      .collection('users')
+      .doc(this.uid)
+      .collection('plots')
+      .add(plot);
+  }
 
   saveGrid(boxPositions: Array<Box>) {
     const event = {
       boxes: boxPositions,
     };
 
-    this.authService.updateUserRecord(this.authService.user.uid, event);
+    this.db
+      .collection('users')
+      .doc(this.uid)
+      .update(event);
   }
 
   loadGrid() {
-    const uid = this.authService.user.uid;
-    const docRef = this.db.collection('users').doc(uid);
+    const docRef = this.db.collection('users').doc(this.uid);
 
     return docRef.get();
+  }
+
+  saveBoxDetails(box: Box) {
+    // this.db
+    //   .collection(`users/`)
+    //   .where('boxes', 'array-contains', box);
   }
 }
