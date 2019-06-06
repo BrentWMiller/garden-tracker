457d269801c2dc0e6b2ba69d25e0daf8e2b9eed7
Brent Miller <brentwmiller93@gmail.com>
2019-06-05 23:38:28 -0400

    updates module imports

--- src/app/plotter/plotter.module.ts
+++ src/app/plotter/plotter.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 // modules
 import { SharedModule } from '../shared/shared.module';
@@ -12,19 +13,23 @@ import { BoxComponent } from './box/box.component';
 
 // services
 import { PlotterService } from './plotter.service';
+import { BoxFormComponent } from './box/box-form/box-form.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PlotFormComponent } from './plot-form/plot-form.component';
 
 export const ROUTES: Routes = [
   {
     path: '',
-    component: GridComponent,
+    component: DashboardComponent,
   },
   // Fallback when no prior routes is matched
   { path: '**', redirectTo: '/', pathMatch: 'full' },
 ];
 
 @NgModule({
-  declarations: [GridComponent, BoxComponent],
-  imports: [CommonModule, RouterModule.forChild(ROUTES), SharedModule, DragDropModule],
+  declarations: [GridComponent, BoxComponent, BoxFormComponent, DashboardComponent, PlotFormComponent],
+  entryComponents: [BoxFormComponent, PlotFormComponent],
+  imports: [CommonModule, RouterModule.forChild(ROUTES), SharedModule, DragDropModule, FormsModule, ReactiveFormsModule],
   providers: [PlotterService],
 })
 export class PlotterModule {}
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -5,10 +5,11 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [],
-  imports: [MatInputModule, MatFormFieldModule, MatToolbarModule, MatSelectModule, MatButtonModule],
-  exports: [MatInputModule, MatFormFieldModule, MatToolbarModule, MatSelectModule, MatButtonModule],
+  imports: [MatInputModule, MatFormFieldModule, MatToolbarModule, MatSelectModule, MatButtonModule, MatDialogModule],
+  exports: [MatInputModule, MatFormFieldModule, MatToolbarModule, MatSelectModule, MatButtonModule, MatDialogModule],
 })
 export class MaterialModule {}
