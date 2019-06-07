f083c0e51f32191ca8b62be5d4add4d8980348b4
Brent Miller <brentwmiller93@gmail.com>
2019-06-06 23:59:52 -0400

    adds demensions; updates with extra fields

--- src/app/plotter/interfaces/plot.interface.ts
+++ src/app/plotter/interfaces/plot.interface.ts
@@ -1,3 +1,13 @@
+import { Box } from './box.interface';
+
 export interface Plot {
   title: string;
+  description?: string;
+  demensions: PlotDemensions;
+  boxes?: Array<Box>;
+}
+
+export interface PlotDemensions {
+  width: number;
+  height: number;
 }
