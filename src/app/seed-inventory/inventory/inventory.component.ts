30fb9e051b3989a0faeb3ddae5aef98797fad326
Brent Miller <brentwmiller93@gmail.com>
2019-06-11 22:55:46 -0400

    Adds seed creation on inventory

--- src/app/seed-inventory/inventory/inventory.component.ts
+++ src/app/seed-inventory/inventory/inventory.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { SeedFormComponent } from '../seed-form/seed-form.component';
+import { Seed } from '../interfaces/seed.interface';
 
 @Component({
   selector: 'gt-inventory',
   templateUrl: './inventory.component.html',
-  styleUrls: ['./inventory.component.scss']
+  styleUrls: ['./inventory.component.scss'],
 })
 export class InventoryComponent implements OnInit {
+  constructor(private dialog: MatDialog) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  openSeedForm() {
+    const dialogRef = this.dialog.open(SeedFormComponent, {
+      width: '960px',
+    });
+
+    dialogRef.componentInstance.saveEvent.subscribe((event) => {
+      this.createSeed(event);
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      dialogRef.componentInstance.saveEvent.unsubscribe();
+    });
   }
 
+  createSeed(event: Seed) {
+    console.log('create seed: ', event);
+  }
 }
