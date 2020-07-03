a8b5e8659d1c9480b4fa5333fcf0d62b5c8ab197
Brent Miller <brentwmiller93@gmail.com>
2020-07-02 23:48:25 -0400

    updates description

--- gt.config.js
+++ gt.config.js
@@ -2,7 +2,7 @@ import { env } from "./env";
 
 export default {
   site_name: "Garden Tracker",
-  description: "Helping hobbyist gardeners track their gardens",
+  description: "Helping hobbyist gardeners track their home gardens",
   firebase: {
     config: env.firebase
   }
