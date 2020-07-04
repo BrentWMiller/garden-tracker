5eec478b46e2be5d7f5040c496593c8146d61986
Brent Miller <brentwmiller93@gmail.com>
2020-07-04 13:02:01 -0400

    Sets up the base navbar

---
+++ components/TheNavbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="fixed bottom-0 w-full px-6 py-4 bg-white">
+    <ul class="flex justify-between">
+      <li class="font-bold" v-for="link in links" :key="link.uri">
+        <nuxt-link :to="link.uri">{{ link.title }}</nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      links: [
+        {
+          uri: "/gardens",
+          title: "Gardens"
+        },
+        {
+          uri: "/plants",
+          title: "Plants"
+        },
+        {
+          uri: "/journal",
+          title: "Journal"
+        },
+        {
+          uri: "/profile",
+          title: "Profile"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style></style>
