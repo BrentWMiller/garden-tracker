c1d3a359847fd0c94bfaedb09ce6fdb65259f7dc
Brent Miller <brentwmiller93@gmail.com>
2020-07-05 23:02:28 -0400

    Adds nested css

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,7 +57,16 @@ export default {
    ** Build configuration
    ** See https://nuxtjs.org/api/configuration-build/
    */
-  build: {},
+  build: {
+    postcss: {
+      // Add plugin names as key and arguments as value
+      // Install them before as dependencies with npm or yarn
+      plugins: {
+        // Disable a plugin by passing false as value
+        "postcss-nested": {}
+      }
+    }
+  },
   router: {
     middleware: "auth-guard"
   },
